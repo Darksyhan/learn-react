3b405891f7182b3a913eeec052292992840d7040
Darksyhan <hansy1310@naver.com>
2026-09-16 13:50:12 +0900

    추가 컴포먼트

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 //(삭제) import logo from "./logo.svg";
 //(삭제) import "./App.css";
 
-import Greeting from "./Greeting";
+import Card from "./components/Card";
+import Greeting from "./components/Greeting";
 
 function App() {
   // logic_로직
@@ -28,6 +29,12 @@ function App() {
     <div className="App">
       Hello World!
       <Greeting />
+      <Greeting />
+      {/**미션 : Card 컴포넌트 만들고, 이자리에서 호출 */}
+      <Card />
+      <Card />
+      <Card />
+      <Card />
     </div>
   );
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div>
+      <img
+        src="https://cf.product-image.s.zigzag.kr/original/d/2026/8/24/2833_202608241452280370_67109.jpeg?width=720&height=720&quality=80&format=webp"
+        alt="지그재그 img"
+      ></img>
+      <p>지그재그 이미지</p>
+    </div>
+  );
+};
+
+export default Card;
