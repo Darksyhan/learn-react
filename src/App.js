28b4bbb010ecc948a074332fd570d43b00d11adf
Darksyhan <hansy1310@naver.com>
2026-09-16 13:20:59 +0900

    컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 //(삭제) import logo from "./logo.svg";
 //(삭제) import "./App.css";
 
+import Greeting from "./Greeting";
+
 function App() {
-  return <div className="App">Hello World!</div>;
+  // logic_로직
+  // 구조분해할당 - object destructuring
+  const animals = ["dog", "cat", "pig"];
+  // const second = animals[1];
+  // const first = animals[1];
+
+  const [first, second] = animals;
+  console.log("🚀 ~ App ~ first:", first);
+  console.log("🚀 ~ App ~ second:", second);
+
+  const todoItem = {
+    todo: "학습하기",
+    isComplete: false,
+  };
+
+  const { todo: todoName, isComplete } = todoItem;
+  console.log("🚀 ~ App ~ isComplete:", isComplete);
+  console.log("🚀 ~ App ~ todo:", todoName);
+
+  // view_문법(html)
+  return (
+    <div className="App">
+      Hello World!
+      <Greeting />
+    </div>
+  );
 }
 
 export default App;
